b6c79029327200ce4e18e9e5b4c00c83d782d9c4
crogger <118250093+crogger@users.noreply.github.com>
2024-08-23 09:03:20 +0000

    creacion del componente titulo

---
+++ Persa/src/componentes/Titulo.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+function Titulo(){
+    return(
+        <>
+           <h1 className="titulo-empresa">
+            Persa
+           </h1>
+        </>
+    );
+}
+
+export default Titulo;
